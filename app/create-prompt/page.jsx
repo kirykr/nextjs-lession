3852d7b12457edc094a18c25562644cdeae40582
kirykr <kirymeas@gmail.com>
2023-09-09 21:40:31 +0700

    CRUD prompt

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,7 +1,51 @@
+'use client';
+
+import { useState } from "react"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
+  const [submitting, setSumitting] = useState(false);
+  const [post, setPost] = useState({
+    prompt: '',
+    tag: ''
+  });
+
+  const createPrompt = async (e) => {
+    e.preventDefault();
+    setSumitting(true);
+
+    try {
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+       if(response.ok) {
+        router.push('/');
+       }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSumitting(false)
+    }
+  }
   return (
-    <div>CreatePrompt</div>
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
   )
 }
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,62 @@
+'use client';
+import { useState, useEffect } from "react";
+
+import PromptCard from "./PromptCard";
+
+const PromptCardList = ({ data, handleTagClick }) => {
+  return (
+    <div className="mt-16 prompt_layout">
+      {
+        data.map((post) => (
+          <PromptCard
+            key={post._id}
+            post={post}
+            handleTagClick={handleTagClick}
+          />
+        ))
+      }
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+
+      setPosts(data)
+    }
+
+    fetchPosts();
+  }, []);
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <form
+        className="relative w-full flext-center"
+      >
+        <input
+          type="text"
+          className="search_input peer"
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+        />
+      </form>
+
+      <PromptCardList
+        data={posts}
+        handleTagClick={() => {}}
+      />
+    </section>
   )
 }
 
